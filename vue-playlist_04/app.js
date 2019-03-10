ee6f8bd83709d013360c6afd6e4d519b99d7f2c2
Yhchdev <630673943@qq.com>
2019-03-10 11:55:17 +0800

    v-if,v-for循环遍历

---
+++ vue-playlist_04/app.js
@@ -0,0 +1,25 @@
+//实例化vue对象
+new Vue({
+	el:"#vue-app",
+	data:{
+		arrarylist:["onsenUI","Vue.js","Spring"],
+		users:[
+			{name:"harry",age:23},
+			{name:"luacy",age:20},
+			{name:"jmmcliy",age:18},
+		]
+	},
+	methods:{
+
+
+	},
+	computed:{
+
+	}
+})
+
+/**
+ * el  :element 作用的获取容器元素
+ * data:数据存储
+ * method:定义方法
+ */
